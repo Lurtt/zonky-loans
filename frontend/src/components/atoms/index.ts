e63952518c8fb9b2c9ad8e8a243ec22b5f30fc0e
Lukas Pospisil <lurtiik@gmail.com>
2019-06-29 14:55:48 +0200

    GH-6 - move colors to atoms

--- frontend/src/components/atoms/index.ts
+++ frontend/src/components/atoms/index.ts
@@ -1,3 +1,4 @@
+export * from './colors'
 export * from './animations'
 export * from './header'
 export * from './root'
--- frontend/src/components/atoms/link/index.tsx
+++ frontend/src/components/atoms/link/index.tsx
@@ -6,6 +6,4 @@ export interface LinkProps extends React.ComponentProps<'a'> {}
 
 export const Link = styled.a<ThemedStyledProps<LinkProps, Theme>>`
   color: ${({ theme }) => theme.colors.cyan};
-  background: ${(props: ThemedStyledProps<LinkProps, Theme>) =>
-    props.theme.colors.cyan};
 `
